57dc8609169e7ac497e9cc14d75abe5fe1806536
willthree123 <willthree123@gmail.com>
2021-09-08 00:03:29 +0800

    final

--- edit.cpp
+++ edit.cpp
@@ -7,11 +7,11 @@
 #endif
 
 #define servo_pin D1 //Servo Motor Pin
-#define openDoorSwitch_pin D2 //Door limit switch pin
+#define openDoorSwitch_pin D6 //Door limit switch pin
 #define LED_PIN D3 //Neopixel pin
 #define NUMPIXELS 1 //Number of Neopixels
 #define ledTimeout 5000
-#define closeDoorTime 250
+#define closeDoorTime 100
 
 // Enter your wifi network name and Wifi Password
 const char* ssid = "LLC-iPAD";
@@ -39,30 +39,37 @@ bool ledStatus;
 const long timeoutTime = 2000;
 
 void initialize() {
-	openDoor();
+  openDoor();
 
-	for (int i = 0; i < 5; i++) {
-		pixels.setPixelColor(0, pixels.Color(70, 70, 0));
-		pixels.show();
-		delay(100);
-		pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-		pixels.show();
-		delay(100);
-	}
-	delay(1000);
-	closeDoor();
-	pixels.setPixelColor(0, pixels.Color(70, 70, 0));
-	pixels.show();
-	ledStatus=HIGH;
-	ledPreviousTime =  millis();
+  for (int i = 0; i < 5; i++) {
+    pixels.setPixelColor(0, pixels.Color(70, 70, 0));
+    pixels.show();
+    delay(100);
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+    delay(100);
+  }
+  delay(1000);
+  closeDoor();
+  pixels.setPixelColor(0, pixels.Color(70, 70, 0));
+  pixels.show();
+  ledStatus=HIGH;
+  ledPreviousTime =  millis();
 }
 
 void openDoor() {
-	while (digitalRead(openDoorSwitch_pin) == HIGH) {
-    myservo.write(45);//OPEN DOOR
+  while (digitalRead(openDoorSwitch_pin) == HIGH) {
+    myservo.write(150);//OPEN DOOR
     Serial.println("SERVO MOVING");
     pixels.setPixelColor(0, pixels.Color(70, 70, 0));
     pixels.show();
+} 
+delay(220);
+while (digitalRead(openDoorSwitch_pin)==HIGH){
+  myservo.write(130);//OPEN DOOR
+  Serial.println("SERVO MOVING");
+  pixels.setPixelColor(0, pixels.Color(70, 70, 0));
+  pixels.show();    
 }
 pixels.setPixelColor(0, pixels.Color(0, 70, 0));
 pixels.show();
@@ -72,31 +79,31 @@ ledPreviousTime =  millis();
 }
 
 void closeDoor() {
-	myservo.write(150);
-	pixels.setPixelColor(0, pixels.Color(70, 70, 0));
-	pixels.show();
-	delay(closeDoorTime);
-	pixels.setPixelColor(0, pixels.Color(70, 0, 0));
-	pixels.show();
-	ledStatus=HIGH;
-	ledPreviousTime =  millis();
-	myservo.write(90);
+  myservo.write(45);
+  pixels.setPixelColor(0, pixels.Color(70, 70, 0));
+  pixels.show();
+  delay(closeDoorTime);
+  pixels.setPixelColor(0, pixels.Color(70, 0, 0));
+  pixels.show();
+  ledStatus=HIGH;
+  ledPreviousTime =  millis();
+  myservo.write(90);
 }
 void ledTimeoutCheck(){
-	if(millis()-ledPreviousTime >= ledTimeout && ledStatus!= LOW){
-		pixels.setPixelColor(0, pixels.Color(0, 0, 0));
-		pixels.show();
-		Serial.println("led off");
-		ledStatus=LOW;
-	}
+  if(millis()-ledPreviousTime >= ledTimeout && ledStatus!= LOW){
+    pixels.setPixelColor(0, pixels.Color(0, 0, 0));
+    pixels.show();
+    Serial.println("led off");
+    ledStatus=LOW;
+  }
 }
 void pinSetup() {
-	myservo.attach(servo_pin);
-	myservo.writeMicroseconds(1500);
-	pinMode(openDoorSwitch_pin, INPUT_PULLUP);
-	pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
-	pixels.clear();
-	myservo.write(90);
+  myservo.attach(servo_pin);
+  myservo.writeMicroseconds(1500);
+  pinMode(openDoorSwitch_pin, INPUT_PULLUP);
+  pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixels.clear();
+  myservo.write(90);
 }
 
 void webserverHandle(){
@@ -109,7 +116,7 @@ String currentLine = ""; // make a String to hold incoming data from the client
 currentTime = millis();
 previousTime = currentTime;
 while (client.connected() && currentTime - previousTime <= timeoutTime) { // loop while the client's connected
-	currentTime = millis(); 
+  currentTime = millis(); 
 if (client.available()) { // if there's bytes to read from the client,
 char c = client.read(); // read a byte, then
 Serial.write(c); // print it out the serial monitor
@@ -117,53 +124,53 @@ header += c;
 if (c == '\n') { // if the byte is a newline character
 // if the current line is blank, you got two newline characters in a row.
 // that's the end of the client HTTP request, so send a response:
-	if (currentLine.length() == 0) {
+  if (currentLine.length() == 0) {
 // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
 // and a content-type so the client knows what's coming, then a blank line:
-		client.println("HTTP/1.1 200 OK");
-		client.println("Content-type:text/html");
-		client.println("Connection: close");
-		client.println();
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-type:text/html");
+    client.println("Connection: close");
+    client.println();
 
 // turns the GPIOs on and off
-		if (header.indexOf("GET /2/on") >= 0 && doorOpenState=="off") {
-			Serial.println("RED LED is on");
-			doorOpenState = "on";
-			openDoor();
-		} else if (header.indexOf("GET /2/off") >= 0 && doorOpenState=="on") {
-			Serial.println("RED LED is off");
-			doorOpenState = "off";
-			closeDoor();
-		} 
+    if (header.indexOf("GET /2/on") >= 0 && doorOpenState=="off") {
+      Serial.println("RED LED is on");
+      doorOpenState = "on";
+      openDoor();
+    } else if (header.indexOf("GET /2/off") >= 0 && doorOpenState=="on") {
+      Serial.println("RED LED is off");
+      doorOpenState = "off";
+      closeDoor();
+    } 
 
 // Display the HTML web page
-		client.println("<!DOCTYPE html><html>");
-		client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-		client.println("<link rel=\"icon\" href=\"data:,\">");
+    client.println("<!DOCTYPE html><html>");
+    client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+    client.println("<link rel=\"icon\" href=\"data:,\">");
 // CSS to style the on/off buttons 
-		client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-		client.println(".buttonRed { background-color: #ff0000; border: none; color: white; padding: 16px 40px; border-radius: 60%;");
-		client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-		client.println(".buttonOff { background-color: #77878A; border: none; color: white; padding: 16px 40px; border-radius: 70%;");
-		client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}</style></head>");
+    client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+    client.println(".buttonRed { background-color: #ff0000; border: none; color: white; padding: 16px 40px; border-radius: 60%;");
+    client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+    client.println(".buttonOff { background-color: #77878A; border: none; color: white; padding: 16px 40px; border-radius: 70%;");
+    client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}</style></head>");
 
 // Web Page Heading
-		client.println("<body><h1>My LED Control Server</h1>");
+    client.println("<body><h1>My LED Control Server</h1>");
 
 // Display current state, and ON/OFF buttons for GPIO 2 Red LED 
-		client.println("<p>Red LED is " + doorOpenState + "</p>");
+    client.println("<p>Red LED is " + doorOpenState + "</p>");
 // If the doorOpenState is off, it displays the OFF button 
-		if (doorOpenState=="off") {
-			client.println("<p><a href=\"/2/on\"><button class=\"button buttonOff\">OFF</button></a></p>");
-		} else {
-			client.println("<p><a href=\"/2/off\"><button class=\"button buttonRed\">ON</button></a></p>");
-		} 
+    if (doorOpenState=="off") {
+      client.println("<p><a href=\"/2/on\"><button class=\"button buttonOff\">OFF</button></a></p>");
+    } else {
+      client.println("<p><a href=\"/2/off\"><button class=\"button buttonRed\">ON</button></a></p>");
+    } 
 // The HTTP response ends with another blank line
-		client.println();
+    client.println();
 // Break out of the while loop
-		break;
+    break;
 } else { // if you got a newline, then clear currentLine
-	currentLine = "";
+  currentLine = "";
 }
 } else if (c != '\r') { // if you got anything else but a carriage return character,
 currentLine += c; // add it to the end of the currentLine
@@ -179,33 +186,33 @@ Serial.println("");
 }
 }
 void WiFiSetup(){
-	// Connect to Wi-Fi network with SSID and password
-	Serial.print("Connecting to ");
-	Serial.println(ssid);
-	WiFi.begin(ssid, password);
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-	}
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
 // Print local IP address and start web server
-	Serial.println("");
-	Serial.println("WiFi connected.");
-	Serial.println("IP address: ");
-	Serial.println(WiFi.localIP());
-	server.begin();
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
 void setup() {
-	Serial.begin(115200);
+  Serial.begin(115200);
 // Initialize the output variables as outputs
-	pinSetup();
+  pinSetup();
   //Serial.println("pinSetup Finished.");
-	initialize();
+  initialize();
 //WiFi Setup
-	WiFiSetup();
+  WiFiSetup();
 }
 
 void loop(){
-	webserverHandle();
-	ledTimeoutCheck();
+  webserverHandle();
+  ledTimeoutCheck();
 
 }
